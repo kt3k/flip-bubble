a45cbb9d703bb1f0e4e9120efbbd9f41e5395d49
Yoshiya Hinosawa <stibium121@gmail.com>
2017-01-22 00:04:18 +0900

    refactor: switch to standard

--- src.js
+++ src.js
@@ -1,104 +1,95 @@
 
 (function ($, capsid) {
-    'use strict'
+  'use strict'
 
-    var DEFAULT_WIDTH = 200
-    var DEFAULT_HEIGHT = 100
-    var DEFAULT_COLOR = '#115588'
-    var DEFAULT_CHIP_HEIGHT = 12
-    var DEFAULT_CHIP_DISTANCE = 15
+  var DEFAULT_WIDTH = 200
+  var DEFAULT_HEIGHT = 100
+  var DEFAULT_COLOR = '#115588'
+  var DEFAULT_CHIP_HEIGHT = 12
+  var DEFAULT_CHIP_DISTANCE = 15
 
-    /**
-     * MultiflipBubble is the component of the bubble which opens above the target element.
-     *
-     * @example
-     *     <div class="multiflip-bubble" m="10" n="5" width="200" height="100" color="#115588" chip-distance="12" chip-height="15">
-     *
-     * The attributes means:
-     * - attr {number} m The horizontal partition number
-     * - attr {number} n The vertical partition number
-     * - attr {number} width The width of the bubble
-     * - attr {number} height The height of the bubble
-     * - attr {string} color The color of the bubble
-     * - attr {number} chip-distance The distance between the bottom of the chip and the top of the bubble target (speaker)
-     * - attr {number} chip-height The height of the chip under the bubble
-     */
-    class MultiflipBubble {
+  /**
+   * MultiflipBubble is the component of the bubble which opens above the target element.
+   *
+   * @example
+   *     <div class="multiflip-bubble" m="10" n="5" width="200" height="100" color="#115588" chip-distance="12" chip-height="15">
+   *
+   * The attributes means:
+   * - attr {number} m The horizontal partition number
+   * - attr {number} n The vertical partition number
+   * - attr {number} width The width of the bubble
+   * - attr {number} height The height of the bubble
+   * - attr {string} color The color of the bubble
+   * - attr {number} chip-distance The distance between the bottom of the chip and the top of the bubble target (speaker)
+   * - attr {number} chip-height The height of the chip under the bubble
+   */
+  class MultiflipBubble {
 
-        __init__ () {
-            var elem = this.$el
-            var target = this.target = elem.data('target')
+    __init__ () {
+      var elem = this.$el
+      var target = this.target = elem.data('target')
 
-            this.$parent = elem.parent()
+      this.$parent = elem.parent()
 
-            this.x = target.position().left + target.width() / 2
-            this.y = target.position().top
+      this.x = target.position().left + target.width() / 2
+      this.y = target.position().top
 
-            this.w = +elem.attr('width') || DEFAULT_WIDTH // component parameter
-            this.h = +elem.attr('height') || DEFAULT_HEIGHT // component parameter
+      this.w = +elem.attr('width') || DEFAULT_WIDTH // component parameter
+      this.h = +elem.attr('height') || DEFAULT_HEIGHT // component parameter
 
-            this.color = elem.attr('color') || DEFAULT_COLOR // component parameter
+      this.color = elem.attr('color') || DEFAULT_COLOR // component parameter
 
-            this.chipHeight = elem.attr('chip-height') || DEFAULT_CHIP_HEIGHT // component paramter
-            this.distance = elem.attr('chip-distance') || DEFAULT_CHIP_DISTANCE // component parameter
+      this.chipHeight = elem.attr('chip-height') || DEFAULT_CHIP_HEIGHT // component paramter
+      this.distance = elem.attr('chip-distance') || DEFAULT_CHIP_DISTANCE // component parameter
 
-            this.init(elem)
+      this.init(elem)
+    }
 
-        }
+    createChip () {
+      // The small chip under the bubble
+      return $('<div />').css({
 
-        createChip() {
+        position: 'absolute',
+        bottom: '-' + this.chipHeight * 2 + 'px',
+        left: this.w / 2 - Math.floor(this.chipHeight / 1.5),
+        width: 0,
+        height: 0,
+        borderWidth: this.chipHeight + 'px ' + Math.floor(this.chipHeight / 1.5) + 'px',
+        borderColor: 'transparent',
+        borderStyle: 'solid',
+        borderTop: 'solid ' + this.chipHeight + 'px ' + this.color,
+        borderTopColor: this.color,
+        opacity: 0
 
-            // The small chip under the bubble
-            return $('<div />').css({
+      })
+    }
 
-                position: 'absolute',
-                bottom: '-' + this.chipHeight * 2 + 'px',
-                left: this.w / 2 - Math.floor(this.chipHeight / 1.5),
-                width: 0,
-                height: 0,
-                borderWidth: this.chipHeight + 'px ' + Math.floor(this.chipHeight / 1.5) + 'px',
-                borderColor: 'transparent',
-                borderStyle: 'solid',
-                borderTop: 'solid ' + this.chipHeight + 'px ' + this.color,
-                borderTopColor: this.color,
-                opacity: 0
+    init (elem) {
+      elem.css({
+        position: 'absolute',
+        left: (this.x - this.w / 2) + 'px',
+        top: (this.y - this.h - this.chipHeight - this.distance) + 'px'
+      })
+        .attr({
+          bgcolor: this.color
+        })
+        .width(this.w)
+        .height(this.h)
+        .append(this.createChip())
+        .cc('multiflip')
+    }
 
-            })
+    show () {
+      return this.elem.cc.get('multiflip').show()
+    }
 
-        }
-
-        init(elem) {
-
-            elem.css({
-                position: 'absolute',
-                left: (this.x - this.w / 2) + 'px',
-                top: (this.y - this.h - this.chipHeight - this.distance) + 'px'
-            })
-            .attr({
-                bgcolor: this.color
-            })
-            .width(this.w)
-            .height(this.h)
-            .append(this.createChip())
-            .cc('multiflip')
-
-        }
-
-        show() {
-
-            return this.elem.cc.get('multiflip').show()
-
-        }
-
-        hide() {
-
-            return this.elem.cc.get('multiflip').hide()
-
-        }
+    hide () {
+      return this.elem.cc.get('multiflip').hide()
+    }
 
     }
 
-    capsid.def('multiflip-bubble', MultiflipBubble)
+  capsid.def('multiflip-bubble', MultiflipBubble)
 
     /**
      * @param {jQuery} content The content
@@ -111,26 +102,23 @@
      * @param {number} opts.chipDistance The distance between the bottom of the chip and the top of the bubble target (speaker)
      * @param {string} opts.color The color of the bubble
      */
-    $.fn.multiflipBubble = function (content, opts) {
+  $.fn.multiflipBubble = function (content, opts) {
+    opts = opts || {}
 
-        opts = opts || {}
+    return capsid.make('multiflip-bubble', $('<div />', {
+      attr: {
+        m: opts.m,
+        n: opts.n,
+        width: opts.width,
+        height: opts.height,
+        color: opts.color,
+        'chip-height': opts.chipHeight,
+        'chip-distance': opts.chipDistance
+      },
+      data: {target: this},
+      insertAfter: this,
+      append: content.css({opacity: 0, position: 'relative'})
 
-        return capsid.make('multiflip-bubble', $('<div />', {
-            attr: {
-                m: opts.m,
-                n: opts.n,
-                width: opts.width,
-                height: opts.height,
-                color: opts.color,
-                'chip-height': opts.chipHeight,
-                'chip-distance': opts.chipDistance
-            },
-            data: {target: this},
-            insertAfter: this,
-            append: content.css({opacity: 0, position: 'relative'})
-
-        })[0])
-
-    }
-
+    })[0])
+  }
 }(jQuery, capsid))
